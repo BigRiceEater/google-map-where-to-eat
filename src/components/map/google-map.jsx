35f5a0ebb17fe08bfe62b3c79951dfde7267f9bd
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-20 14:48:55 +0800

    Extra managing current location to another service

--- src/components/map/google-map.jsx
+++ src/components/map/google-map.jsx
@@ -1,47 +1,58 @@
 import React, { Component } from 'react';
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 import ServicePlaces from './service-places';
+import ServiceLocation from './service-location';
 import config from '@root/google.config';
 
 class GoogleMap extends Component {
   constructor(props) {
     super(props);
-
-    const defaultLocation = {
-      lat: 22.337118,
-      lng: 114.148293
-    };
-
-    const currentLocation = defaultLocation;
+    const location = new ServiceLocation();
 
     const currentLocationMarker = {
-      position: currentLocation,
+      position: location.get(),
       title: 'This your current location',
       name: 'You'
     };
 
     const markers = [currentLocationMarker];
 
-    this.state = { currentLocation, currentLocationMarker, markers };
+    this.state = {
+      services: { location },
+      currentLocationMarker,
+      markers
+    };
   }
 
   handleMapReady = (mapProps, map) => {
     const { google } = mapProps;
-    const { currentLocation } = this.state;
-    const placesService = new ServicePlaces(google, map, currentLocation);
+    const { location } = this.state.services;
+    const places = new ServicePlaces(google, map, location.get());
+    this.setState({ services: { location, places } });
+  };
 
-    this.setState({ placesService });
+  handleDragend = () => {
+    console.log('the map was dragged');
+    // handle center of map changed here?
+  };
+
+  handleMapClick = () => {
+    console.log('Only fired if no marker or infowindow was clicked');
+    // can use this to handle choosing your location.
   };
 
   render() {
+    const { currentLocationMarker, markers } = this.state;
     return (
       <div>
         <Map
           google={this.props.google}
           zoom={14}
-          initialCenter={this.state.currentLocation}
-          onReady={this.handleMapReady}>
-          {this.state.markers.map((marker) => (
+          initialCenter={currentLocationMarker.position}
+          onReady={this.handleMapReady}
+          onDragend={this.handleDragend}
+          onClick={this.handleMapClick}>
+          {markers.map((marker) => (
             <Marker
               key={marker.name}
               name={marker.name}
---
+++ src/components/map/service-location.js
@@ -0,0 +1,10 @@
+export default class ServiceLocation {
+  constructor() {}
+
+  get() {
+    return {
+      lat: 22.337118,
+      lng: 114.148293
+    };
+  }
+}
