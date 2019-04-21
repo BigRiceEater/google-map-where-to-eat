82d2e4254a87dc8c9b56f3e33ced636843ff1a2b
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-21 14:13:48 +0800

    Add bulma

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import './styles/main.scss';
+import 'bulma/css/bulma.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
