8925a0ff6b7c8dae8f356cca7950f6c52d605119
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-21 15:37:26 +0800

    Make google map fit into its parent container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import GoogleMap from './components/map/google-map';
 import Panel from './components/side-panel/panel';
-import { relative } from 'path';
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div
-        id='app-container'
-        style={{ position: 'relative', width: '200px', height: '200px' }}>
-        <GoogleMap />
+      <div className='columns is-gapless' style={{ height: '100%' }}>
+        <div className='column is-three-quarters'>
+          <div
+            id='map-container'
+            style={{ position: 'relative', width: '100%', height: '100%' }}>
+            <GoogleMap />
+          </div>
+        </div>
+        <div className='column is-one-quarter'>
+          <Panel />
+        </div>
       </div>
     );
   }
